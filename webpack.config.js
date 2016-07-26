14428186df9b6e42527ee18d39a7ae7e2acee659
mikeynova <newfoundland7@gmail.com>
2016-07-26 15:52:21 -0700

    initial skeleton

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  entry: [
+    './client/index.js'
+  ],
+  output: {
+    path: __dirname + '/client/dist',
+    filename: "bundle.js"
+  },
+  module: {
+    loaders: [
+      { 
+        test: /\.js$/, 
+        exclude: /node_modules/, 
+        loader: "babel", 
+        query: { presets: ['es2015', 'react'] }
+      }
+    ]
+  }
+}

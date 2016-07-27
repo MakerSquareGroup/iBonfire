b0ec6f4b682f683ef91bf57dd8403ea665a18234
Ryan Morris <ryanmorris.j@gmail.com>
2016-07-27 15:18:13 -0700

    [Update] adding routes and backend structure

---
+++ server/routes/bonfireRoutes.js
@@ -0,0 +1,12 @@
+const controllers = require('../controllers/bonfireCtrl.js');
+const router = require('express').Router();
+
+for(let route in controllers) {
+	router.route(route)
+	.get(controllers[route].get)
+	.post(controllers[route].post)
+	.put(controllers[route].put)
+	.delete(controllers[route].delete);
+}
+
+module.exports = router;
--- server/server.js
+++ server/server.js
@@ -20,6 +20,11 @@ app.use(express.static('../client'));
 // Using the cors npm in this way enables all cors requests
 app.use(cors());
 
+// Routes for endpoints
+app.use('api/user', userRoutes);
+// app.use('api/bonfire', bonfireRoutes);
+app.use('api/location', locationRoutes);
+
 app.get('/', (request, response) => {
 	response.sendFile(path.resolve(__dirname, '../client', 'index.html'));
 });

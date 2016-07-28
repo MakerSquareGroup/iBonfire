ea901ecfaddf61b9baead34e3f735dd80fe6c5ad
seanml <i.r.sean@gmail.com>
2016-07-27 18:42:24 -0700

    [bugfix/config] updated start script

--- client/index.js
+++ client/index.js
@@ -8,6 +8,7 @@ import thunk from 'redux-thunk';
 import reducer from './public/reducers';
 
 import App from './public/components/App';
+import Login from './public/components/FBLogin';
 
 const createStoreWithMiddleWare = applyMiddleware(thunk)(createStore);
 const store = createStoreWithMiddleWare(reducer);
@@ -15,7 +16,9 @@ const store = createStoreWithMiddleWare(reducer);
 const router = (
   <Provider store={store}>
     <Router history={browserHistory}>
-      <Route path="/" component={App} />
+      <Route path="/" component={App} >
+        <IndexRoute component={Login} />
+      </Route>
     </Router>
   </Provider>
 )

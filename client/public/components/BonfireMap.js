1a0c96ce641f99f5cd7a59ae072eef65f6a4a16a
mikeynova <newfoundland7@gmail.com>
2016-07-30 10:20:50 -0700

    [Feature] Changed map package to react-google-maps, and renamed google map component

---
+++ client/public/components/BonfireMap.js
@@ -0,0 +1,106 @@
+import React, {Component} from 'react';
+// import {Gmaps, Marker, InfoWindow, Circle} from 'react-gmaps';
+import {GoogleMapLoader, GoogleMap, Marker} from "react-google-maps";
+import { KEY } from '../../apiKeys';
+
+// const coords = {
+//   lat: 51.5258541,
+//   lng: -0.08040660000006028
+// };
+
+export default class BonfireMap extends Component {
+	constructor(props){
+		super(props)
+		this.state = {
+
+		}
+	}
+
+	// onMapCreated(map) {
+	// 	map.setOptions({
+	// 		disableDefaultUI: true
+	// 	});
+	// 	marker = new google.maps.Marker({
+ //          map: map,
+ //          draggable: true,
+ //          animation: google.maps.Animation.DROP,
+ //          position: {lat: 59.327, lng: 18.067}
+ //    });
+ //    marker.addListener('click', toggleBounce);
+	// };
+
+	// onDragEnd(e) {
+	// 	console.log('onDragEnd', e);
+	// };
+
+	// onCloseClick() {
+	// 	console.log('onCloseClick');
+	// };
+
+	// onClick(e) {
+	// 	console.log('onClick', e);
+	// };
+			// <div id="GoogleMap">
+			// 	<Gmaps
+			//         width={'100%'}
+			//         height={'100%'}
+			//         lat={coords.lat}
+			//         lng={coords.lng}
+			//         zoom={12}
+			//         loadingMessage={'Be happy'}
+			//         params={{v: '3.exp', key: KEY}}
+			//         onMapCreated={this.onMapCreated}>
+			//         <Marker
+			//           lat={coords.lat}
+			//           lng={coords.lng}
+			//           draggable={true}
+			//           onDragEnd={this.onDragEnd} />
+			//         <InfoWindow
+			//           lat={coords.lat}
+			//           lng={coords.lng}
+			//           content={'Hello, React :)'}
+			//           onCloseClick={this.onCloseClick} />
+			//         <Circle
+			//           lat={coords.lat}
+			//           lng={coords.lng}
+			//           radius={500}
+			//           onClick={this.onClick} />
+	  //     		</Gmaps>
+   //    		</div>
+
+
+	render(){
+		return (
+			<div>
+				<section style={{height: "100%"}}>
+		      <GoogleMapLoader
+		        containerElement={
+		          <div
+		            {...props.containerElementProps}
+		            style={{
+		              height: "100%",
+		            }}
+		          />
+		        }
+		        googleMapElement={
+		          <GoogleMap
+		            ref={(map) => console.log(map)}
+		            defaultZoom={3}
+		            defaultCenter={{ lat: -25.363882, lng: 131.044922 }}
+		            onClick={props.onMapClick}
+		          >
+		            {props.markers.map((marker, index) => {
+		              return (
+		                <Marker
+		                  {...marker}
+		                  onRightclick={() => props.onMarkerRightclick(index)} />
+		              );
+		            })}
+		          </GoogleMap>
+		        }
+		      />
+		    </section>
+	    </div>
+    )
+	}
+}
--- client/public/components/Home.js
+++ client/public/components/Home.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import GoogleMap from './GoogleMap';
+import BonfireMap from './BonfireMap';
 import DragAndDrop from './DragAndDrop';
 
 
@@ -7,33 +7,7 @@ export default class Home extends Component {
 	render() {
     return (
       <div id="Home">
-        <GoogleMap/>
-      <div id="map">
-        <Gmaps
-          width={'800px'}
-          height={'600px'}
-          lat={coords.lat}
-          lng={coords.lng}
-          zoom={12}
-          loadingMessage={'Be happy'}
-          params={{v: '3.exp', key: KEY}}
-          onMapCreated={this.onMapCreated}>
-          <Marker
-            lat={coords.lat}
-            lng={coords.lng}
-            draggable={true}
-            onDragEnd={this.onDragEnd} />
-          <InfoWindow
-            lat={coords.lat}
-            lng={coords.lng}
-            content={'Hello, React :)'}
-            onCloseClick={this.onCloseClick} />
-          <Circle
-            lat={coords.lat}
-            lng={coords.lng}
-            radius={500}
-            onClick={this.onClick} />
-        </Gmaps>
+        <BonfireMap/>
       </div>
     );
   }

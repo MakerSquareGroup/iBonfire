a046fcb6fce991d8fc4e686dc626a98dee99e3c6
Ryan Morris <ryanmorris.j@gmail.com>
2016-08-01 21:02:22 -0700

    [Fix] bonfire enpoints now work, fixed errors on backend

--- server/controllers/bonfireCtrl.js
+++ server/controllers/bonfireCtrl.js
@@ -8,7 +8,7 @@ module.exports = {
 			console.log('Recieved GET at /api/bonfire');
 			console.log('Sending all bonfires');
 
-			Bonfire.findAllBonfire()
+			Bonfire.findAllBonfires()
 				.then((bonfires) => {
 					if (bonfires.length === 0) {
 						console.log('There are no bonfires');
@@ -19,7 +19,6 @@ module.exports = {
 					}
 				});
 		},
-
 		post: (req, res) => {
 			console.log('Recieved POST at api/bonfire');
 			console.log('Creating bonfire');
@@ -47,46 +46,66 @@ module.exports = {
 					}
 				});
 		},
-
-		put: (req, res) => {
-			console.log("Received PUT at /api/user/");
-			res.end("Received PUT at /api/user");
-		},
-
-		delete: (req, res) => {
-			console.log("Received DELETE at /api/user/");
-			res.end("Received Delete at /api/user");
-		}
-	},
-
-	'/:bonfire_specs': {
-		get: (req, res) => {
-			console.log('Recieved GET at /api/bonfire');
-			console.log('Sending all bonfires');
-
-			Bonfire.findAllBonfire()
-				.then((bonfires) => {
-					if (bonfires.length === 0) {
-						console.log('There are no bonfires');
-						res.end('There are no bonfires');
-					} else {
-						console.log('We got bonfires!');
-						res.send(bonfires);
-					}
-				});
-		},
-
-		post: (req, res) => {
-			console.log('Recieved POST at api/bonfire');
-			res.end('Recieved POST at api/bonfire');
-			
-		},
-
 		put: (req, res) => {
 			console.log("Received PUT at /api/bonfire/");
 			res.end("Received PUT at /api/bonfire");
 		},
+		delete: (req, res) => {
+			console.log("Received DELETE at /api/bonfire/");
+			res.end("Received Delete at /api/bonfire");
+		}
+	},
 
+	// Bonfire specs can be in the form of bonfire id or latitude&longitude so that a user can be searched by either id or coordinates
+	// For example:
+
+	// Search by bonfire Id:
+	// /api/bonfire/1
+
+	// Search by coordinates
+	// /api/bonfire/34.024212&-118.496475
+
+	'/:bonfire_specs': {
+		get: (req, res) => {
+			console.log('Recieved GET at /api/bonfire');
+			console.log(req.params, "This is the params object in bonfires");
+
+			// This function checks for the type of prop you are searching for
+			var getParams = checkParamsBonfire(req.params.bonfire_specs);
+
+			if (Array.isArray(getParams)) {
+				Bonfire.findrBonfireByLocation(getParams[0], getParams[1])
+					.then((bonfire) => {
+						if (!bonfire) {
+							console.log('Bonfire at ' + getParams + ' does not exist!');
+							res.end('Bonfire at' + getParams + ' does not exist!');
+						} else {
+							console.log('Found the bonfire you are looking for!');
+							res.send(bonfire);
+						}
+					});
+			} else {
+				Bonfire.findBonfireById(getParams)
+					.then((bonfire) => {
+						if (!bonfire) {
+							console.log('Bonfire with bonfire id ' + getParams + ' does not exist!');
+							res.end('Bonfire with bonfire id ' + getParams + ' does not exist!');
+						} else {
+							console.log('Found the bonfire you are looking for!');
+							res.send(bonfire);
+						}
+					});
+			}
+		},
+		post: (req, res) => {
+			console.log('Recieved POST at api/bonfire');
+			res.end('Recieved POST at api/bonfire');
+
+		},
+		put: (req, res) => {
+			console.log("Received PUT at /api/bonfire/");
+			res.end("Received PUT at /api/bonfire");
+		},
 		delete: (req, res) => {
 			console.log("Received DELETE at /api/bonfire/");
 			res.end("Received Delete at /api/bonfire");
@@ -94,6 +113,28 @@ module.exports = {
 	}
 
 };
+
+// The below functions, checkParamBonfire and seperateLatLongBonfire are used to determine the proper endpoint
+// based on coordinates or Facebook ID
+
+checkParamsBonfire = getParams => {
+	var reg = /[&]/g;
+	if (getParams.match(reg)) {
+		console.log("This GET request is for a bonfire at a coordinate");
+		return seperateLatLongBonfire(getParams);
+	} else {
+		console.log("This GET request finds a bonfire based on the bonfire id");
+		return getParams;
+	}
+};
+
+seperateLatLongBonfire = getParams => {
+	var reg = /[&]/;
+	var coords = getParams.split(reg);
+
+	return coords;
+};
+
 
 var postmanbonfire = {
 	"name": "Ryan's",

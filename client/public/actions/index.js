72b911caec45967943c2d1a777db3959fd2c5149
seanml <i.r.sean@gmail.com>
2016-08-03 16:47:22 -0700

    [Fix] Fix git syncing issue with actions index file

--- client/public/actions/index.js
+++ client/public/actions/index.js
@@ -47,8 +47,8 @@ export function addMarker(data) {
   })
 }
 
-export function changeClassName(boollean) {
-  if(boollean === true) {
+export function changeBonfireModalClassName(animation) {
+  if(animation === "fadeOut") {
     return ({
       type: CHANGE_CLASSNAME,
       payload:
@@ -62,19 +62,22 @@ export function changeClassName(boollean) {
       }
     })
   }
-  return ({
-    type: CHANGE_CLASSNAME,
 
-    payload: 
-    { 
-      class: 
-         { 
-           bonfireModal: 'bonfireModal', 
-           modelTextBox: 'modelTextBox',
-           showModal: 'showModal'
-         }
-    }
-  })
+  if(animation === "fadeIn"){
+    return ({
+      type: CHANGE_CLASSNAME,
+      payload: 
+      { 
+        class: 
+           { 
+             bonfireModal: 'bonfireModal', 
+             modelTextBox: 'modelTextBox',
+             showModal: 'showModal'
+           }
+      }
+    })
+  }
+
 }
 
 export function addUser(user, picture) {

990fd1ee497df18c4f88bbe0a2cc039e28a2d0dc
mikeynova <newfoundland7@gmail.com>
2016-08-02 19:55:02 -0700

    [Feature] Completed show model on added bonfire

--- client/public/actions/index.js
+++ client/public/actions/index.js
@@ -14,6 +14,7 @@ export function addMarker(data) {
 }
 
 export function changeClassName() {
+  console.log('inside changeCLassName action creator', CHANGE_CLASSNAME);
   return ({
     type: CHANGE_CLASSNAME,
     payload: {bonfireModal: 'bonfireModal'}
--- client/public/reducers/changeClassReducer.js
+++ client/public/reducers/changeClassReducer.js
@@ -1,4 +1,4 @@
-import CHANGE_CLASSNAME from '../actions/index';
+import { CHANGE_CLASSNAME } from '../actions/index';
 
 const INITIAL_STATE = {
   hidden: 'hidden'
@@ -6,9 +6,11 @@ const INITIAL_STATE = {
 
 export default function(state = INITIAL_STATE, action) {
   console.log(action.type, 'what is the payload')
+  console.log('this is change_classname:', CHANGE_CLASSNAME);
   switch(action.type) {
     case CHANGE_CLASSNAME:
       console.log(state, 'STATE IN REDUCER CLASNAME')
+      console.log('this is being returned:', action.payload.bonfireModal);
       return action.payload.bonfireModal;
   }
   console.log(state, 'sstate in jibfdjdksfbj');

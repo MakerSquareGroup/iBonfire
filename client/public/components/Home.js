6a160c4f21b542a1cf2b63ad5b7090dc2a39882d
Dailen Spencer <dailenspencer@gmail.com>
2016-08-05 14:12:54 -0700

    [Feature] Add map to profile page animation

--- client/public/components/Home.js
+++ client/public/components/Home.js
@@ -6,17 +6,46 @@ import DragAndDrop from './DragAndDrop';
 import { connect } from 'react-redux';
 import * as actions from '../actions/index';
 
+import ProfilePage from './ProfilePage/ProfilePage';
+
 class Home extends Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      mapClass : 'Map',
+      profilePageClass: 'ProfilePage'
+    }
+    this.transitionToProfilePage = this.transitionToProfilePage.bind(this);
+    this.transitionToMap = this.transitionToMap.bind(this);
+  }
+
+  componentDidMount(){
+   
+  }
+
+  transitionToProfilePage(){
+    this.setState({
+      mapClass: 'Map FadeOutMap',
+      profilePageClass: 'ProfilePage FadeInProfilePage'
+    })
+  }
+
+  transitionToMap(){
+    
   }
 
 	render() {
     return (
       <div id="Home">
-      	<Navigation/>
-        <BonfireMap/>
-        <ProfileButton/>
+      	<div className={this.state.mapClass}>
+          <Navigation/>
+          <BonfireMap/>
+          <ProfileButton transitionToProfilePage={this.transitionToProfilePage}/>
+        </div>
+        <div className={this.state.profilePageClass}>
+          <ProfilePage/>
+        </div>
       </div>
     );
   }
---
+++ client/public/components/ProfilePage/ProfilePage.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+export default class ProfilePage extends React.Component {
+	constructor(props){
+		super(props);
+		this.state = {
+			mapButtonClass: 'MapButtonSmall',
+      mapButtonImageClass: 'MapImageSmall MapImageSmallAnimation'
+		}
+	}
+
+
+
+	render(){
+		return(
+			<div>
+				<div className="BonfireHolder">
+					<div className="BonfireEntry">
+						<img src="" />
+					</div>
+				</div>
+				<div className="MapButtonSmall">
+					<img  className="MapImageSmall MapImageSmallAnimation" src='http://www.appelsiini.net/assets/2008/5/26/tartu.png'/>
+				</div>
+			</div>
+		)
+	}
+}

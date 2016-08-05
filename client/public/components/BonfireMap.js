e79d201850adea289dc1246ab2d3b3485d26084e
mikeynova <newfoundland7@gmail.com>
2016-08-05 10:37:25 -0700

    [Feature] Hide modal when clicking outside of the modal

--- client/public/components/BonfireMap.js
+++ client/public/components/BonfireMap.js
@@ -9,159 +9,163 @@ import { connect } from 'react-redux';
 import * as actions from '../actions/index';
 
 class BonfireMap extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      location: {
-        lat: this.props.location.lat, 
-        lng: this.props.location.lng
-      }
-    }
-    this.handleMouseOver = this.handleMouseOver.bind(this);
-    this.handleMouseOut = this.handleMouseOut.bind(this);
-  }
+	constructor(props) {
+		super(props);
+      console.log(this.props, 'hisdflhsdf')
 
-  componentWillMount() {
-    this.props.getCurrentUser();
-    this.props.getLocation();
-    this.props.getMarkers();
-  }
+		this.state = {
+			location: {
+				lat: this.props.location.lat, 
+				lng: this.props.location.lng
+			}
+		}
+		this.handleMouseOver = this.handleMouseOver.bind(this);
+		this.handleMouseOut = this.handleMouseOut.bind(this);
+	}
 
-  componentWillReceiveProps(nextProps) {
-    const location = this.props.location;
-    const nextLocation = nextProps.location;
-    const search = this.props.search.searchCoords;
-    const nextSearch = nextProps.search.searchCoords;
+	componentWillMount() {
+		this.props.getCurrentUser();
+		this.props.getLocation();
+		this.props.getMarkers();
+	}
 
-    if(location.lat !== nextLocation.lat || location.lng !== nextLocation.lng) {
-      this.setState({
-        location: {
-          lat: nextLocation.lat,
-          lng: nextLocation.lng
-        }
-      });
-    }
+	componentWillReceiveProps(nextProps) {
+		const location = this.props.location;
+		const nextLocation = nextProps.location;
+		const search = this.props.search.searchCoords;
+		const nextSearch = nextProps.search.searchCoords;
 
-    if(search.latitude !== nextSearch.latitude || search.longitude !== nextSearch.longitude) {
-      this.setState({
-        location: {
-          lat: nextSearch.latitude,
-          lng: nextSearch.longitude
-        }
-      });
-    }
+		if(location.lat !== nextLocation.lat || location.lng !== nextLocation.lng) {
+			this.setState({
+				location: {
+					lat: nextLocation.lat,
+					lng: nextLocation.lng
+				}
+			});
+		}
 
-    if(this.props.markers.length < nextProps.markers.length) {
-      this.props.getMarkers();
-    }
-  }
+		if(search.latitude !== nextSearch.latitude || search.longitude !== nextSearch.longitude) {
+			this.setState({
+				location: {
+					lat: nextSearch.latitude,
+					lng: nextSearch.longitude
+				}
+			});
+		}
 
-  handleMapClick(event) {
-    let lat = event.latLng.lat();
-    let long = event.latLng.lng();
-    let markerObject = {
-      position: { lat: lat, lng: long }
-    }
-    if(this.props.changeClass.bonfireModal !== 'hidden') {
-      return;
-    }
+		if(this.props.markers.length < nextProps.markers.length) {
+			this.props.getMarkers();
+		}
+	}
 
-    this.setState({
-      location: {
-        lat: lat,
-        lng: long
-      }
-    });
+	handleMapClick(event) {
+		let lat = event.latLng.lat();
+		let long = event.latLng.lng();
+		let markerObject = {
+			position: { lat: lat, lng: long }
+		}
+		const changed = this.props.changeClass.changed;
+		
+		if(changed.bonfireModal !== 'hidden') {
+			return this.props.changeBonfireModalClassName("fadeOut")
+		}
 
-    this.props.setCurrentMarker(markerObject);
-    this.props.changeBonfireModalClassName("fadeIn");
-  }
+		this.setState({
+			location: {
+				lat: lat,
+				lng: long
+			}
+		});
 
-  logout() {
-    if(!window.isLoaded) {
-      facebookInit();
-    }
-    facebookLogout();
-  }
+		this.props.setCurrentMarker(markerObject);
+		this.props.changeBonfireModalClassName("fadeIn");
+	}
 
-  newCenter() {
-    let lat = this.refs.googleMap.getCenter().lat();
-    let lng = this.refs.googleMap.getCenter().lng();
+	logout() {
+		if(!window.isLoaded) {
+			facebookInit();
+		}
+		facebookLogout();
+	}
 
-    this.setState({
-      location: {
-        lat: lat, 
-        lng: lng
-      }
-    });
-  }
+	newCenter() {
+		let lat = this.refs.googleMap.getCenter().lat();
+		let lng = this.refs.googleMap.getCenter().lng();
 
-  handleMouseOver(marker) {
-    if(this.props.changeClass.bonfireModal === 'hidden') {
-      this.props.getHoverMarker(marker);
-      this.props.displayHoverModal();
-    }
-  }
+		this.setState({
+			location: {
+				lat: lat, 
+				lng: lng
+			}
+		});
+	}
 
-  handleMouseOut(event) {
-    this.props.hideHoverModal();
-  }
+	handleMouseOver(marker) {
+		if(this.props.changeClass.bonfireModal === 'hidden') {
+			this.props.getHoverMarker(marker);
+			this.props.displayHoverModal();
+		}
+	}
 
-  render() {
-    return (
-      <GoogleMapLoader
-        containerElement={
-          <div style={{ height: "100%" }} />
-        }
-        googleMapElement={
-          <GoogleMap
-            ref="googleMap"
-            defaultZoom={15}
-            onCenterChanged={this.newCenter.bind(this)}
-            center={this.state.location}
-            defaultCenter={this.props.location}
-            onClick={this.handleMapClick.bind(this)}
-          >
+	handleMouseOut(event) {
+		this.props.hideHoverModal();
+	}
 
-          {this.props.markers.map((marker, index) => {
-            let position = {
-              lat: Number(marker.latitude),
-              lng: Number(marker.longitude)
-            }
-            return (
-            <Marker
-            icon='../media/BonFire.png'
-            position={position}
-            defaultAnimation={2}
-            key={index}
-            value={marker}
-            onMouseover={() => this.handleMouseOver(marker)}
-            onMouseout={() => this.handleMouseOut(marker)}
-            />
-          )
-          })}
+	render() {
+		return (
+			<GoogleMapLoader
+			  containerElement={
+			    <div style={{ height: "100%" }} />
+			  }
+			  googleMapElement={
+			    <GoogleMap
+						ref="googleMap"
+						defaultZoom={15}
+			      onCenterChanged={this.newCenter.bind(this)}
+			      center={this.state.location}
+			      defaultCenter={this.props.location}
+			      onClick={this.handleMapClick.bind(this)}
+			    >
 
-            <BonfireModal />
-            <MarkerModal />
+			   	{this.props.markers.map((marker, index) => {
+	    			let position = {
+	    				lat: Number(marker.latitude),
+	    				lng: Number(marker.longitude)
+	    			}
+	    			return (
+	    			<Marker
+		    		icon='../media/BonFire.png'
+		    		position={position}
+		    		defaultAnimation={2}
+		    		key={index}
+		    		value={marker}
+		    		onMouseover={() => this.handleMouseOver(marker)}
+		    		onMouseout={() => this.handleMouseOut(marker)}
+		    		/>
+	    		)
+	    		})}
 
-          </GoogleMap>
-        }
-      />
+						<BonfireModal />
+						<MarkerModal />
+
+			    </GoogleMap>
+		  	}
+			/>
     )
-  }
+	}
 }
 
 const mapStateToProps = state => {
-  return {
-    markers: state.markers,
-    users: state.users,
-    location: state.location,
-    changeClass: state.changeClass,
-    facebook: state.facebook,
-    search: state.search,
-    currentMarker: state.currMarker,
-    hoverMarker: state.hoverMarker
-  }
+	return {
+		markers: state.markers,
+		users: state.users,
+		location: state.location,
+		changeClass: state.changeClass,
+		facebook: state.facebook,
+		search: state.search,
+		currentMarker: state.currMarker,
+		hoverMarker: state.hoverMarker
+	}
 }
 
 export default connect(mapStateToProps, actions)(BonfireMap);

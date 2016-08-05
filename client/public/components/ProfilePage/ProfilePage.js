0bed08bffcc0ecc422a96b39337541d4120b5b85
Dailen Spencer <dailenspencer@gmail.com>
2016-08-05 15:28:14 -0700

    [Style] Set template for profile page

--- client/public/components/ProfilePage/ProfilePage.js
+++ client/public/components/ProfilePage/ProfilePage.js
@@ -12,18 +12,41 @@ export default class ProfilePage extends React.Component {
 
 
 	render(){
+		
+
+		var latinText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+
+
+
+
+
+
+
+
 		return(
 			<div>
+				<div className="ProfilePageTop">
+					<div className="ProfilePageLeft">
+						<div className="ProfilePageName">
+							Dailen Spencer
+						</div>
+						<div className="ProfilePageUserInfo">
+							{latinText}
+						</div>
+					</div>
+					<div className="ProfilePageMiddle">
+
+					</div>
+					<div className="ProfilePageRight">
+						<div className="ProfilePageStats">
+							
+						</div>
+						<div className="ProfilePagePoints">
+							67,536
+						</div>
+					</div>
+				</div>
 				<div className="BonfireHolder">
-					<div className="BonfireEntry">
-						<img className="BonfireEntryImage" src="../../media/Bonfire_2.png"/>
-					</div>
-					<div className="BonfireEntry">
-						<img className="BonfireEntryImage" src="../../media/Bonfire_2.png"/>
-					</div>
-					<div className="BonfireEntry">
-						<img className="BonfireEntryImage" src="../../media/Bonfire_2.png"/>
-					</div>
 					<div className="BonfireEntry">
 						<img className="BonfireEntryImage" src="../../media/Bonfire_2.png"/>
 					</div>

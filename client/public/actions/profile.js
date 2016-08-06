02499af294f0089365c774c6285f36ba329e09b4
Dailen Spencer <dailenspencer@gmail.com>
2016-08-06 16:55:04 -0700

    [Feature] Add user bonfires & profile picture to profile page

--- client/public/actions/profile.js
+++ client/public/actions/profile.js
@@ -3,10 +3,9 @@ import axios from 'axios';
 export const GET_USER_BONFIRES = 'GET_USER_BONFIRES';
 
 export function getUserBonfires(userId){
-  const grabBonfiresDB = axios.get('/user/' + userId);
+  const grabBonfiresDB = axios.get('/bonfire/join_bonfire/' + userId);
   return (dispatch) => {
     return grabBonfiresDB.then((response) => {
-      console.log(response, "response");
       dispatch({
         type: GET_USER_BONFIRES,
         payload: {

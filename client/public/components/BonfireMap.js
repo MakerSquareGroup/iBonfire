f60fad92df7dd0635697773e57e5ddb6a1c67bd2
seanml <i.r.sean@gmail.com>
2016-08-15 14:54:56 -0700

    [Refactor] Removed unused code

--- client/public/components/BonfireMap.js
+++ client/public/components/BonfireMap.js
@@ -194,60 +194,51 @@ class BonfireMap extends Component {
 	}
 
 	render() {
-		if(this.state.location.lat && this.state.location.lng) {
-			return (
-				<GoogleMapLoader
-				  containerElement={
-				    <div style={{ height: "100%" }} />
-				  }
-				  googleMapElement={
-				    <GoogleMap
-							ref="googleMap"
-							defaultZoom={15}
-				      onCenterChanged={this.newCenter.bind(this)}
-				      center={this.state.location}
-				      defaultCenter={this.props.location}
-				      onClick={this.handleMapClick.bind(this)}
-				      defaultOptions={{
-			          styles: [{"elementType":"geometry","stylers":[{"hue":"#ff4400"},{"saturation":-68},{"lightness":-4},{"gamma":0.72}]},{"featureType":"road","elementType":"labels.icon"},{"featureType":"landscape.man_made","elementType":"geometry","stylers":[{"hue":"#0077ff"},{"gamma":3.1}]},{"featureType":"water","stylers":[{"hue":"#00ccff"},{"gamma":0.44},{"saturation":-33}]},{"featureType":"poi.park","stylers":[{"hue":"#44ff00"},{"saturation":-23}]},{"featureType":"water","elementType":"labels.text.fill","stylers":[{"hue":"#007fff"},{"gamma":0.77},{"saturation":65},{"lightness":99}]},{"featureType":"water","elementType":"labels.text.stroke","stylers":[{"gamma":0.11},{"weight":5.6},{"saturation":99},{"hue":"#0091ff"},{"lightness":-86}]},{"featureType":"transit.line","elementType":"geometry","stylers":[{"lightness":-48},{"hue":"#ff5e00"},{"gamma":1.2},{"saturation":-23}]},{"featureType":"transit","elementType":"labels.text.stroke","stylers":[{"saturation":-64},{"hue":"#ff9100"},{"lightness":16},{"gamma":0.47},{"weight":2.7}]}]
-			        }}
-				    >
+		return (
+			<GoogleMapLoader
+			  containerElement={
+			    <div style={{ height: "100%" }} />
+			  }
+			  googleMapElement={
+			    <GoogleMap
+						ref="googleMap"
+						defaultZoom={15}
+			      onCenterChanged={this.newCenter.bind(this)}
+			      center={this.state.location}
+			      defaultCenter={this.props.location}
+			      onClick={this.handleMapClick.bind(this)}
+			      defaultOptions={{
+		          styles: [{"elementType":"geometry","stylers":[{"hue":"#ff4400"},{"saturation":-68},{"lightness":-4},{"gamma":0.72}]},{"featureType":"road","elementType":"labels.icon"},{"featureType":"landscape.man_made","elementType":"geometry","stylers":[{"hue":"#0077ff"},{"gamma":3.1}]},{"featureType":"water","stylers":[{"hue":"#00ccff"},{"gamma":0.44},{"saturation":-33}]},{"featureType":"poi.park","stylers":[{"hue":"#44ff00"},{"saturation":-23}]},{"featureType":"water","elementType":"labels.text.fill","stylers":[{"hue":"#007fff"},{"gamma":0.77},{"saturation":65},{"lightness":99}]},{"featureType":"water","elementType":"labels.text.stroke","stylers":[{"gamma":0.11},{"weight":5.6},{"saturation":99},{"hue":"#0091ff"},{"lightness":-86}]},{"featureType":"transit.line","elementType":"geometry","stylers":[{"lightness":-48},{"hue":"#ff5e00"},{"gamma":1.2},{"saturation":-23}]},{"featureType":"transit","elementType":"labels.text.stroke","stylers":[{"saturation":-64},{"hue":"#ff9100"},{"lightness":16},{"gamma":0.47},{"weight":2.7}]}]
+		        }}
+			    >
 
-				   	{this.state.markers.map((marker, index) => {
-		    			let position = {
-		    				lat: Number(marker.latitude),
-		    				lng: Number(marker.longitude)
-		    			}
-		    			const ref = `marker_${index}`;
-		    			return (
-		    			<Marker
-			    		icon='../media/BonFire.png'
-			    		position={position}
-			    		defaultAnimation={2}
-			    		key={index}
-			    		ref={ref}
-			    		value={marker}
-			    		onMouseover={() => this.openModal(marker)}
-			    		>
-			    			{marker.showInfo ? this.renderInfoWindow(ref, marker) : null }
+			   	{this.state.markers.map((marker, index) => {
+	    			let position = {
+	    				lat: Number(marker.latitude),
+	    				lng: Number(marker.longitude)
+	    			}
+	    			const ref = `marker_${index}`;
+	    			return (
+	    			<Marker
+		    		icon='../media/BonFire.png'
+		    		position={position}
+		    		defaultAnimation={2}
+		    		key={index}
+		    		ref={ref}
+		    		value={marker}
+		    		onMouseover={() => this.openModal(marker)}
+		    		>
+		    			{marker.showInfo ? this.renderInfoWindow(ref, marker) : null }
 
-			    		</Marker>
-		    		)
-		    		})}
+		    		</Marker>
+	    		)
+	    		})}
 
-							<BonfireModal />
-				    </GoogleMap>
-			  	}
-				/>
-	    )
-		} else {
-			return (
-			  <div className="spinner">
-	        <div className="double-bounce1"></div>
-	        <div className="double-bounce2"></div>
-	      </div>
-      )
-		}
+						<BonfireModal />
+			    </GoogleMap>
+		  	}
+			/>
+    )
 	}
 }
 

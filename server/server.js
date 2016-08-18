b8a75a74b49345defb8f0e528b42dfd7269eed88
seanml <i.r.sean@gmail.com>
2016-08-18 09:16:59 -0700

    [Pull] Work in progress on fixing sockets

--- server/server.js
+++ server/server.js
@@ -9,8 +9,8 @@ const express_enforces_ssl = require('express-enforces-ssl');
 const contentLength = require('express-content-length-validator');
 
 const app = express();
-const https = require('https').Server(app);
-const io = require('socket.io')(https);
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
 require('./sockets/socketHelper')(io);
 
 // For database access and creation.
@@ -37,8 +37,8 @@ app.use(express.static(__dirname + '/../client/public'));
 app.use(contentLength.validateMax());
 
 // Enforces HTTPs connections on incoming requests. Use when deployed.
-app.enable('trust proxy');
-app.use(express_enforces_ssl());
+// app.enable('trust proxy');
+// app.use(express_enforces_ssl());
 
 // Middleware for setting headers.
 app.use(cors());
@@ -66,7 +66,7 @@ app.get('*', (req,res) => {
 
 app.set('port', process.env.PORT || 8080);
 
-https.listen(app.get('port'), () => {
+http.listen(app.get('port'), () => {
   db.ensureSchema();
   console.log(moment().format('h:mm:ss a') + ': Server is Listening on port', app.get('port'));
 });

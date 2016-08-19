dfc461c3156b4c94363c78e254670c183e6deecc
Ryan Morris <ryanmorris.j@gmail.com>
2016-08-19 15:03:22 -0700

    [Pull] Added and styled tutorial guide

--- client/public/components/About.js
+++ client/public/components/About.js
@@ -1,5 +1,9 @@
 import $ from 'jquery'; 
 import React, { Component } from 'react';
+import RaisedButton from 'material-ui/RaisedButton';
+import { browserHistory } from 'react-router';
+import TutorialCarousel from './TutorialCarousel';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 export default class About extends Component {
 
@@ -21,7 +25,7 @@ export default class About extends Component {
             <div className="featurette">
                 <img className="featurette-image img-circle img-responsive pull-right" src="http://p1cdn4static.sharpschool.com/UserFiles/Servers/Server_758978/Image/2013-14/Main%20Calendar%20Graphics/2013%20Homecoming%20Bonfire.gif"></img>
                 <h2 className="featurette-heading">Why iBonfire?
-                    <span className="text-muted">Well, why the hell not.</span>
+                    <span className="text-muted"> Well, why the hell not.</span>
                 </h2>
                 <p className="lead">iBonfire is designed so you, the user, can focus on what is important in life, bonfires. What are bonfires? Bonfires are places of greeting, socializing and change. Light a bonfire to signal other like minded bonfirees or join an existing one and let the living begin!</p>
             </div>
@@ -29,24 +33,30 @@ export default class About extends Component {
             <hr className="featurette-divider"></hr>
      
             <div className="featurette" id="services">
-                <div className="featurette-image img-circle img-responsive pull-left">
-
-                </div>
                 <h2 className="featurette-heading">Getting Started.
-                    <span className="text-muted">You should probably start here...</span>
+                    <span className="text-muted"> You should probably start here...</span>
+
                 </h2>
-                <p className="lead">Click on the reel to the left to go through our super simple tutorial and you will be a bonfire master in no time!</p>
+                <p className="lead">Click on through the slide below to go through our super simple bonfire indoctrin.. *cough* guide and you will be a bonfire master in no time!</p>
+                <div className="tutorialContainer">
+                    <TutorialCarousel />
+                </div>   
             </div>
 
             <hr className="featurette-divider"></hr>
 
             <div className="featurette" id="contact">
-                <img className="featurette-image img-circle img-responsive pull-right" src="http://placehold.it/500x500"></img>
                 <h2 className="featurette-heading">That's it!
-                    <span className="text-muted">What are you waiting for?</span>
+                    <span className="text-muted"> What are you waiting for?</span>
                 </h2>
-                <p className="lead">Continue scrolling down to meet the team or click the big button to get started!</p>
+                <p className="lead">Continue scrolling down to meet the team or click "Get Started" to... well, get started!</p>
+                <MuiThemeProvider>
+                    <RaisedButton className='bigbtn' label="Get Started" onClick={() => browserHistory.push('/login')} >
+                    </RaisedButton>
+                </MuiThemeProvider>
             </div>
+
+            <hr className="featurette-divider"></hr>
 
             <hr className="featurette-divider"></hr>
 

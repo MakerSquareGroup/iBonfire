bc739f2026eb8a92996d736378a480089c3b6f26
seanml <i.r.sean@gmail.com>
2016-08-21 23:27:42 -0700

    [Pull] Chat online status work in progress, unimplemented until completion

--- server/sockets/socketHelper.js
+++ server/sockets/socketHelper.js
@@ -1,20 +1,26 @@
 const moment = require('moment');
 
+let clients = [];
+
 module.exports = (io) => {
   io.on('connect', (socket) => {
     socket.once('disconnect', () => {
       console.log('Disconnected.');
     });
 
-    socket.on('joinChat', (bonfireId) => {
-      console.log('Joined room ' + bonfireId);
+    socket.on('joinChat', (params) => {
+      console.log('Joined room ' + params.bonId);
+      if(clients.indexOf(params.name) === -1) {
+        clients.push(params.name);
+      }
       socket.emit('Received socket id of: ', socket.id);
-      socket.join('Room' + bonfireId);
+      socket.join(params.bonId);
+      console.log('Connected clients:', clients);
     });
 
     socket.on('leaveChat', (bonfireId) => {
       console.log('Leaving room ' + bonfireId);
-      socket.leave('Room' + bonfireId);
+      socket.leave(bonfireId);
     });
 
     socket.on('newMessage', (msg) => {
